9d27750c86315ae5855d64b98bc670b0c892092f
hayfafahaad <61270375+hayfafahaad@users.noreply.github.com>
2023-02-08 16:26:38 +0300

    up

---
+++ Lab/HaifaFahad_Lab1.php
@@ -0,0 +1,18 @@
+<?php
+echo "<h1> Haifa Fahad, 201912120 </h1>";
+echo $n = 1 ;
+echo "<br>";
+echo $s = "1";
+echo "<br>";
+echo $n1= $n.$s;
+echo "<br>";
+echo $n2 = $n1. "10 little penguins";  
+echo "<br>";
+echo $n3 = "hello";
+echo "<br>";
+echo $n4 = " world";
+echo "<br>";
+echo $n5 = $n3.$n4;
+echo "<br>";
+
+?>
